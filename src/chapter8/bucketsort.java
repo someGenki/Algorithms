8493593d60fba5ebaa3e9b48d6340e621416676a
小夕立 <40381052+someGenki@users.noreply.github.com>
2019-07-15 16:52:55 +0800

    writeBucketSort

--- src/chapter8/bucketsort.java
+++ src/chapter8/bucketsort.java
@@ -1,4 +1,12 @@
 package chapter8;
 
+/**
+ * 2019年7月15日
+ * 算法导论第八章:桶排序
+*/
 public class bucketsort {
+    private static int[] arr1 = {16, 21, 5, 49, 33, 456, 327, 56, 65, 234};
+    public static void main(String[] args) {
+
+    }
 }
